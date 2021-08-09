be20bb67a68ab3c3345fad212cb81bc683393236
Anh-vu <anhuvdinh@gmail.com>
2021-08-09 17:28:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let multiply;
+let num1 = 1;
+let num2 = 62;
+
+multiply = num1*num2;
+
+
+let random = Math.floor(Math.random()*10)+1;
+
+let num3 = 4;
+let num4 = 1;
+let mod = num3/num4;
+
+let max;
+max = Math.max(1,2,3,4,5,20);
